b7437cb21e72a2be96a95303dfdff67c16862933
Precious <jkpresh@gmail.com>
2023-08-11 16:23:35 -0700

    Added 8-print_base16.c file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,10 @@ int main(void)
 	char a;
 	int b;
 
-    	for (b = 0; b < 10; b++)
-        	putchar(b + '0');
-
+	for (b = 0; b < 10; b++)
+		putchar(b + '0');
 	for (a = 'a'; a <= 'f'; a++)
-        	putchar(a);
-
-    	putchar('\n');
-    	return (0);
+		putchar(a);
+	putchar('\n');
+	return (0);
 }
