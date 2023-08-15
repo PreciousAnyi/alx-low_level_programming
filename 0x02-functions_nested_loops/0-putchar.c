9b4cacc590d07e4798011b84c5c7cc8191e7880d
Precious <jkpresh@gmail.com>
2023-08-15 08:07:29 -0700

    Added 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main- Entry pointt
  *
@@ -6,6 +6,14 @@
  */
 int main(void)
 {
-	printf("_putchar");
+	char str[] = "_putchar";
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		putchar(i);
+
+		i++;
+	}
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,1 @@
+int putchat(char n);
