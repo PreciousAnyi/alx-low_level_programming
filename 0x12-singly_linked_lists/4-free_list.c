7382824424a47b68334b102e374d0fa278f2aa18
preciousanyi <jkpresh@gmail.com>
2023-11-16 17:27:56 -0800

    Added 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next_node;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
+}
+
